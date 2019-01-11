a43081e5c2631915b3eac124bd1905297fcf65b7
Marcos Ruginski <marcosrobertoruginski@gmail.com>
2019-01-11 10:32:51 -0200

    Adicionando os primeiros arquivos PHP

---
+++ SQL.php
@@ -0,0 +1,52 @@
+<?php 
+
+class SQL extends PDO {
+
+	private $conexao;
+
+	public function __construct(){
+
+		$this->conexao = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+	}
+
+	private function setParams($statement , $parametros = array() ){
+
+		foreach ($parametros as $key => $value) {
+			
+			$this->setParam($key,$value);
+
+		}
+
+	}
+
+	private function setParam($statement , $key , $value){
+
+		$statement->bindParam($key , $value);
+
+	}
+
+	public function query($rawQuery, $parametros = array() ){
+
+		$stmt = $this->conexao->prepare($rawQuery);	
+
+		$this->setParams($stmt , $parametros);
+
+		$stmt->execute();
+
+		return $stmt;
+
+	}
+
+	public function select($rawQuery , $params = array() ) :array
+	{
+
+		$stmt = $this->query($rawQuery , $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+?>
